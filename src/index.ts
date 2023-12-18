64df9d1698fa0407c613271de6bf47816b0a6951
Tomer Edelsberg <tomeredel@gmail.com>
2023-12-19 00:40:11 +0200

    implement DB connection

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,13 @@ import dotenv from "dotenv";
 import { error } from "console";
 import bodyParser from "body-parser";
 import cors from "cors";
+import { Todo, connectDB } from "./db";
 
+const http = require("http");
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
-type mockDBType = { todos: { description: string; id: number }[] };
-const mockDB: mockDBType = {
-  todos: [
-    { description: "feed my cat", id: 1.7 },
-    { description: "feed my dog", id: 2 },
-    { description: "feed my dog", id: 3 },
-  ],
-};
 
 app.use(
   cors({
@@ -28,12 +22,28 @@ app.use((req, res, next) => {
 });
 app.use(bodyParser.json({ limit: "50mb" }));
 
+const serverOptions: any = {};
+
+// serverOptions.server = http.createServer(app);
+// serverOptions.port = 4000;
+// serverOptions.protocol = "HTTP";
+
+connectDB("mongodb://127.0.0.1:27017/todos")
+  .then(() => {
+    console.log("connected");
+  })
+  .catch(() => {});
+
 app.get("/", (req: Request, res: Response) => {
   res.send("My Server");
 });
 
-app.get("/todos", (req: Request, res: Response) => {
-  res.send({ todos: mockDB.todos });
+app.get("/todos", async (req: Request, res: Response) => {
+  const todos = await Todo.find(
+    {},
+    { id: true, description: true, _id: false }
+  );
+  res.send({ todos });
 });
 
 app.post("/todo", async (req: Request, res: Response) => {
@@ -44,10 +54,12 @@ app.post("/todo", async (req: Request, res: Response) => {
     if (!description) {
       throw new Error("invalid inputs");
     }
-    mockDB.todos.push({ description, id: Math.random() });
+
+    const todo = new Todo({ description, id: Math.random() });
+    await todo.save();
     res.status(201).json({
       message: "create OK",
-      added: { description, id: Math.random() },
+      added: { description, id: todo.id },
     });
   } catch (err: any) {
     res.status(500).json({
@@ -56,29 +68,29 @@ app.post("/todo", async (req: Request, res: Response) => {
   }
 });
 
-app.put("/todos", async (req: Request, res: Response) => {
-  try {
-    console.log(req.body);
-    const id = req.body.id;
+// app.put("/todos", async (req: Request, res: Response) => {
+//   try {
+//     console.log(req.body);
+//     const id = req.body.id;
 
-    if (!id) {
-      throw new Error("invalid inputs");
-    }
-    mockDB.todos.map((item) => {
-      if (item.id === id) {
-        item.description = req.body.description;
-      }
-    });
-    res.status(201).json({
-      message: "changed",
-      changeID: req.body.id,
-    });
-  } catch (err: any) {
-    res.status(500).json({
-      message: err.message,
-    });
-  }
-});
+//     if (!id) {
+//       throw new Error("invalid inputs");
+//     }
+//     mockDB.todos.map((item) => {
+//       if (item.id === id) {
+//         item.description = req.body.description;
+//       }
+//     });
+//     res.status(201).json({
+//       message: "changed",
+//       changeID: req.body.id,
+//     });
+//   } catch (err: any) {
+//     res.status(500).json({
+//       message: err.message,
+//     });
+//   }
+// });
 
 app.delete("/todos/:id", async (req: Request, res: Response) => {
   try {
@@ -87,13 +99,11 @@ app.delete("/todos/:id", async (req: Request, res: Response) => {
     if (!id) {
       throw new Error("invalid inputs");
     }
-    const newTodos = mockDB.todos.filter((item) => item.id !== id);
-
-    mockDB.todos = newTodos;
+    const todo = await Todo.deleteOne({ id });
 
     res.status(201).json({
       message: "deleted",
-      deletedID: id,
+      changeID: id,
     });
   } catch (err: any) {
     res.status(500).json({
